a1cfe2568cdf30a6a49569fbd6df31153db95acf
juanjosegonzalezrosales <juanjosegonzalezrosales@gmail.com>
2025-05-08 09:45:16 -0500

    multiplicacion-division

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 #Archivo principal
+
+
+valornumerador=float(input("digite un numero"))
+valordenomiador = 0
+while (valordenomiador==0): 
+    valordenomiador=float(input("digite un numero distinto de cero"))
+print("la multiplicacion es: ", valordenomiador*valornumerador)
+print("la multiplicacion es: ", valornumerador/valordenomiador)
+
