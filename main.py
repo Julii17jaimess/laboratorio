a43c9fd68915b5ab4105c334914dcd99403e6de9
Julii17jaimess <jaimesjulianap@gmail.com>
2025-05-08 09:46:29 -0500

    suma  y resta

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
 #Archivo principal
+n1=float(input("ingresa un número:"))
+n2=float(input("ingresa un segundo número:"))
+suma=n1+n2
+resta=n1-n2
+print("la suma de los números es",suma)
+print("la resta de los números es",resta)
